6136bc0ebb012b70a386f91f85f1dd32bb9cfe86
Sayed Farhad Nabizada <nzfarhad@gmail.com>
2023-06-23 18:22:27 +0500

    first commit

---
+++ ckanext/atrtheme/assets/script.js
@@ -0,0 +1,10 @@
+ckan.module("atrtheme-module", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {},
+  };
+});
---
+++ plugin.py
@@ -0,0 +1,15 @@
+# encoding = utf-8
+from ckan.common import CKANConfi
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+class AtrthemePlugin(plugins.SingletonPlugin):
+    '''Atrtheme theme plugin.'''
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+    def update_config(self, config: CKANConfi):
+        '''Update plugin config'''
+        toolkit.add_template_directory(config, 'templates')
+        #toolkit.add_public_directory(config, 'public')
+        #toolkit.add_resource('public', 'ckanext-atrtheme')
