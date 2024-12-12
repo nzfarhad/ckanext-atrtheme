f11af4326a4f18636c5ed791f4e90e98dc8e4dc0
nzfarhad <nzfarhad@gmail.com>
2024-12-13 00:12:53 +0100

    Added language changes - i18n

--- ckanext/atrtheme/plugin.py
+++ ckanext/atrtheme/plugin.py
@@ -1,13 +1,22 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 
-class AtrthemePlugin(plugins.SingletonPlugin):
+class AtrthemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITranslation)
 
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "atrtheme")
+
+    def update_config_schema(self, schema):
+        ignore_missing = toolkit.get_validator('ignore_missing')
+        schema.update({
+            'ckan.site_custom_text': [ignore_missing, str],
+        })
+        return schema
 
